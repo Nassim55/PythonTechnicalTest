d9f1b7325a23127b020b30bf2dac2fc056dfe775
Nassim55 <nassim.chenouf@outlook.com>
2020-10-20 15:19:31 +0100

    GET request to display the users bonds are now working

--- origin/bonds/views.py
+++ origin/bonds/views.py
@@ -1,15 +1,19 @@
+# Rest framework imports:
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from .serializers import BondSerializer
-
+# Model imports:
 from bonds.models import Bond
 
+# Serializer imports:
+from .serializers import BondSerializer
 
 
+'''Viewset handles user interaction with the Bond model. Allows a user to create bonds and retreive
+bonds that they are authourised to retreive'''
 class BondViewSet(viewsets.ModelViewSet):
     # Defining, the user can only access the methods associated with this view if they are 
     # logged in and have an auth token:
@@ -18,8 +22,18 @@ class BondViewSet(viewsets.ModelViewSet):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
+    # List method allows a user to get all of the bond they have saved in the database
+    # Only allows the user to retreive bonds that they are authorised to get:
+    def list(self, request):
+        requested_bond = Bond.objects.filter(user = request.user)
+        serializer = BondSerializer(requested_bond, many=True)
+        return Response({'response': serializer.data})
+
     # Create method allows a user to create a bond in the database using the Bond model:
     def create(self, request, *args, **kwargs):
+        # Before creating a bond we need to get the legal name from the external API:
+
+        # Creates the bond object oin the database:
         Bond.objects.create(
             user = request.user,
             isin = request.data['isin'],
@@ -31,6 +45,8 @@ class BondViewSet(viewsets.ModelViewSet):
 
         # Returning the response of successfully saved Bond.
         return Response({'response': 'You have successfully saved your bond!'})
+
+    
 
 
 
